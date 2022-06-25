56bbcea7a7101871b0ae419ee9e5a6a82998ac23
eunkyung.lee <eunkyung.lee@verbinteractive.com>
2022-06-25 19:23:30 -0300

    working on menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -10,27 +10,28 @@ import m4 from '../assets/images/m4.jpg';
 
 const Gallery1 = () => {   
   return (
-    <div>
-      <h1 className="gallery-title">The gallery1</h1>
-    <div className="container">
-     <div className="box">
-        <img src={m1} alt="sushi"></img>
-        <span>Salmon Teriyaki</span>
-     </div>
-     <div className="box">
-        <img src={m2} alt="sushi"></img>
-        <span>Green onion Beef roll</span>
-     </div>
-     <div className="box">
-        <img src={m3} alt="sushi"></img>
-        <span>Squid Teriyaki</span>
-     </div>
-     <div className="box">
-        <img src={m4} alt="sushi"></img>
-        <span>Chicken Skewered</span>
-     </div>
-    </div>
-
+    <div className="menu-container">
+      <div className="title-container">
+         <h1 className="gallery-title">The gallery1</h1>
+      </div>
+      <div className="container">
+      <div className="box">
+         <img src={m1} alt="sushi"></img>
+         <span>Salmon Teriyaki</span>
+      </div>
+      <div className="box">
+         <img src={m2} alt="sushi"></img>
+         <span>Green onion Beef roll</span>
+      </div>
+      <div className="box">
+         <img src={m3} alt="sushi"></img>
+         <span>Squid Teriyaki</span>
+      </div>
+      <div className="box">
+         <img src={m4} alt="sushi"></img>
+         <span>Chicken Skewered</span>
+      </div>
+      </div>
     </div>
   )
 }
