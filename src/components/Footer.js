f1d67c70700ebbc1c70be5b1824cad4b57d6ddc5
eunkyung.lee <eunkyung.lee@verbinteractive.com>
2022-06-25 23:50:25 -0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
                 <a href="tel:19024068666" className="number"><b>Call us at (902) 406-8866</b></a>
                 <p className="address-time">1475 Bedford Hwy #111, Bedford, </p>
                 <p className="address-time">NS B4A 3Z5</p>
+                <hr className="footer-line"/>
             </div>
-
             <div className="column" id="second">
                 <div className=" container-middle" id="second">
                     <h1 className="text1">Sushi Maru</h1>     
@@ -24,6 +24,7 @@ const Footer = () => {
                 <p className="address-time"> 11:30 AM - 9:00 PM</p>
                 <p className="address-time">(Last Call 8:30 PM)</p>
                 <p className="footer-title">dine in or takeout or delivery</p>
+                <hr className="footer-line"/>
             </div>
         </div>
     )
