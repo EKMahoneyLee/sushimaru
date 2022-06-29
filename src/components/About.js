a56ed91fb05999fa9121a91fe8d0f50a2bb5beaa
eunkyung.lee <eunkyung.lee@verbinteractive.com>
2022-06-29 15:55:58 -0300

    updating all

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
         <h2 className="about-title">To Eat Is A Necessity, But To Eat Joyously Is An Art </h2>
         <div className="about-section">
          
-          <p className="right">We believe the best joy in life is sharing great food with great people, 
+          <p className="about-right">We believe the best joy in life is sharing great food with great people, 
               which is why we are commited to providing a delectable dining experience! <br/>
               Our food is expertly prepared using carefully 
               selected fresh ingredients and served in a comfortable and friendly atmosphere. <br/>
