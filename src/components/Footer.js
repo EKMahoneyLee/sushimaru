ccd49693087d7d21dbbe1a3208a8c154ead37e08
eunkyung.lee <eunkyung.lee@verbinteractive.com>
2022-06-29 23:41:56 -0300

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
             <div className="text">
                 <h1 className="text1">Sushi </h1>    
                 <span> <img className="logo" src={maru} alt="logo"></img></span>
-                <h1 className="text1"> Maru</h1>     
+                <h1 className="text1">Maru </h1>     
             </div>
-                <p className="copyright">© {new Date().getFullYear()} Sushi Maru. All Rights Reserved.</p>
+            <p className="copyright">© {new Date().getFullYear()} Sushi Maru. All Rights Reserved.</p>
         </div>
     )
 }
